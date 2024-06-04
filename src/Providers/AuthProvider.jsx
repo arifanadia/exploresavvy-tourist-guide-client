4e9944b2befacd31712bcbf0405dd8783d8be1e3
arifanadia <contactarifanadia@gmail.com>
2024-06-04 21:58:02 +0600

    private route are ready

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,28 +1,61 @@
-import { createContext, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
 
 
-
-
-const AuthContext = createContext(null)
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider()
 
 const AuthProvider = () => {
-    const [user,setUser] = useState(null);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
 
-    
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    };
+    const login = (email,password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    const signInWithGoogle = () =>{
+        setLoading(true)
+        return signInWithPopup(auth,googleProvider)
+    }
+    const logOut = () =>{
+        setLoading(true)
+        return signOut(auth)
+    }
 
-    const authInfo ={
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+
+        return () => {
+            return unSubscribe();
+        }
+    }, [])
+
+
+
+
+    const authInfo = {
         user,
-        loader,
+        loading,
         setUser,
         createUser,
         login,
+        signInWithGoogle
         logOut,
         userUpdateProfile
     }
     return (
         <AuthContext.Provider value={authInfo}>
-            
+
         </AuthContext.Provider >
     );
 };
