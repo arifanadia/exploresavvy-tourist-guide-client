04e4434df4d9fc71343b21456a928b8b1d3c1514
arifanadia <contactarifanadia@gmail.com>
2024-08-21 23:39:13 +0600

    update : story details page

--- src/Pages/AllStories/AllStories.jsx
+++ src/Pages/AllStories/AllStories.jsx
@@ -9,76 +9,94 @@ import type5 from '../../assets/blog/sundarban.jpg'
 import type6 from '../../assets/blog/Train20170619140222.jpg'
 import { Link } from "react-router-dom";
 
-
 const AllStories = () => {
     const [touristStories] = useTouristStories();
-    const [blogs] = useBlogs()
+    const [blogs] = useBlogs();
+
     return (
         <div>
             <PageHeader title={'Travel Stories are Always Amazing'} bg={bg}></PageHeader>
-            <div className="md:flex gap-8 max-w-7xl mx-auto my-12">
-                <div className="w-1/3">
+            <div className=" gap-8 max-w-7xl mx-auto my-12 px-4 sm:px-6 lg:px-8">
+                <div className="md:w-1/3 mb-8 md:mb-0">
                     <div className="form-control">
-                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            className="input input-bordered w-full md:w-auto"
+                        />
                     </div>
                     <div className="my-8">
                         <h1 className="text-xl font-satisfy font-semibold">Tour Types</h1>
-                        <p className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center " style={{ backgroundImage: `url(${type2})` }}>Adventures</p>
-                        <p className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center" style={{ backgroundImage: `url(${type3})` }}>Wildlife</p>
-                        <p className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center" style={{ backgroundImage: `url(${type4})` }}>Cultural</p>
-                        <p className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center" style={{ backgroundImage: `url(${type5})` }}>Food</p>
-                        <p className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center" style={{ backgroundImage: `url(${type6})` }}>Relaxation</p>
-                        <p className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center" style={{ backgroundImage: `url(${bg})` }}>Historical</p>
-
+                        <p
+                            className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center"
+                            style={{ backgroundImage: `url(${type2})` }}>
+                            Adventures
+                        </p>
+                        <p
+                            className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center"
+                            style={{ backgroundImage: `url(${type3})` }}>
+                            Wildlife
+                        </p>
+                        <p
+                            className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center"
+                            style={{ backgroundImage: `url(${type4})` }}>
+                            Cultural
+                        </p>
+                        <p
+                            className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center"
+                            style={{ backgroundImage: `url(${type5})` }}>
+                            Food
+                        </p>
+                        <p
+                            className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center"
+                            style={{ backgroundImage: `url(${type6})` }}>
+                            Relaxation
+                        </p>
+                        <p
+                            className="text-xl font-semibold my-2 p-6 bg-no-repeat bg-cover text-white rounded-xl text-center"
+                            style={{ backgroundImage: `url(${bg})` }}>
+                            Historical
+                        </p>
                     </div>
-                    <div className="border border-gray-100 rounded-2xl bg-gray-50 p-6 space-y-4">
+                    <div className=" border border-gray-100 rounded-2xl bg-gray-50 p-6 space-y-4">
                         <h1 className="text-xl font-satisfy font-semibold">Blogs</h1>
-                        {
-                            blogs.map(blog =>
-                                <div key={blog._id} className="flex gap-3 items-center mt-4">
-                                    <img className="w-20 h-20 rounded-xl" src={blog.image} alt="" />
+                        <div className="grid grid-cols-2 md:grid-cols-1">
+                            {blogs.map(blog => (
+                                <div key={blog._id} className="lg:flex gap-3 items-center mt-4 text-center lg:text-left">
+                                    <img className="w-20 h-20 lg:mx-0 mx-auto rounded-xl" src={blog.image} alt="" />
                                     <div>
-                                        <h5 className="font-semibold font-satisfy">{blog.title}</h5>
-                                        <p className="my-3">{blog.date}</p>
+                                        <h5 className="font-semibold font-satisfy mt-2 lg:mt-0 ">{blog.title}</h5>
+                                        <p className="my-3 text-sm">{blog.date}</p>
                                     </div>
                                 </div>
-                            )
-                        }
+                            ))}
+                        </div>
                     </div>
-
                 </div>
-                <div>
-                    <div className="grid md:grid-cols-2 gap-8">
-                        {
-                            touristStories.map(stories =>
-                                <Link key={stories._id} to={`/tourist-stories-details/${stories._id}`}>
-                                    <div className="rounded-xl border border-gray-200 w-96 ">
-                                        <img className="md:w-96 h-64" src={stories.image} alt="" />
-                                        <div className="p-6">
-                                            <div className="flex items-end my-2">
-                                                <p>{stories.start_date}-</p>
-                                                <p>{stories.end_date}</p>
-
-                                            </div>
-                                            <div className="flex items-center gap-3 my-2">
-                                                <img className="w-12 h-12 rounded-full" src={stories.tourist_photo} alt="" />
-                                                <h5>{stories.tourist_name}</h5>
-
-                                            </div>
-
-                                            <h4 className="font-satisfy text-2xl m">{stories.title}</h4>
-                                            <p className="my-4">{stories.description.history}....</p>
+                <div className="md:w-2/3">
+                    <div className="grid gap-8 grid-cols-1 lg:grid-cols-3">
+                        {touristStories.map(stories => (
+                            <Link key={stories._id} to={`/tourist-stories-details/${stories._id}`}>
+                                <div className="rounded-xl border border-gray-200 w-full">
+                                    <img className="w-full h-64 object-cover rounded-t-xl" src={stories.image} alt="" />
+                                    <div className="p-6">
+                                        <div className="flex items-end my-2">
+                                            <p>{stories.start_date} -</p>
+                                            <p>{stories.end_date}</p>
                                         </div>
-
+                                        <div className="flex items-center gap-3 my-2">
+                                            <img className="w-12 h-12 rounded-full" src={stories.tourist_photo} alt="" />
+                                            <h5>{stories.tourist_name}</h5>
+                                        </div>
+                                        <h4 className="font-satisfy text-2xl mb-2">{stories.title}</h4>
+                                        <p className="my-4 text-sm">{stories.description.history}....</p>
                                     </div>
-                                </Link>
-                            )
-                        }
+                                </div>
+                            </Link>
+                        ))}
                     </div>
-
                 </div>
             </div>
-
         </div>
     );
 };
--- src/Pages/StoryDetails/StoryDetails.jsx
+++ src/Pages/StoryDetails/StoryDetails.jsx
@@ -1,57 +1,70 @@
-
 import { IoChatbubbleOutline } from "react-icons/io5";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
 import { useLoaderData } from "react-router-dom";
 import { FacebookIcon, FacebookShareButton } from "react-share";
 
-
-
 const StoryDetails = () => {
     const storyDetails = useLoaderData();
-    console.log(storyDetails);
     const shareUrl = window.location.href;
-    
-    
-
 
     return (
         <div>
-            <div className="bg-no-repeat p-64 bg-cover bg-center text-center"
+            <div 
+                className="bg-no-repeat p-6 sm:p-12  md:p-36 bg-cover bg-center text-center"
                 style={{
                     backgroundImage: `linear-gradient(
-            #0f0c29ED,#302b6336),url(${storyDetails.image})`
-                }}>
-                <h1 className="text-3xl font-maven font-semibold text-white">{storyDetails.title}</h1>
-
+                    #0f0c29ED,#302b6336),url(${storyDetails.image})`
+                }}
+            >
+                <h1 className="text-2xl text-center font-maven font-semibold text-white mb-16 md:mb-0">
+                    {storyDetails.title}
+                </h1>
             </div>
-            <div className="-mt-24">
-                <img className="w-64 h-64 mx-auto rounded-full border-4 border-gray-200" src={storyDetails.tourist_photo} alt="" />
+            <div className="-mt-20 sm:-mt-24">
+                <img 
+                    className="w-32 h-32 sm:w-48 sm:h-48 md:w-64 md:h-64 mx-auto rounded-full border-4 border-gray-200" 
+                    src={storyDetails.tourist_photo} 
+                    alt="" 
+                />
             </div>
-            <div className="max-auto text-center my-6" >
-                <h3 className="font-satisfy font-semibold text-xl">By {storyDetails.tourist_name}</h3>
-                <p className="my-2">Travel Story Writer</p>
-                <p className="my-2">{storyDetails.start_date} - {storyDetails.end_date}</p>
-                <p className="my-2">{storyDetails.destination}</p>
-                <div className="border-t border-t-yellow my-3 w-1/2 mx-auto"></div>
-                <div className="flex justify-between w-2/5 mx-auto my-4 text-xl">
-                    <p className="flex gap-2 items-center"><MdOutlineRemoveRedEye /> 2050</p>
-                    <button><FacebookShareButton url={shareUrl}>
-                        <FacebookIcon size={40} round/>
-                    </FacebookShareButton></button>
-                    <p className="flex gap-2 items-center"><IoChatbubbleOutline /> 150</p>
+            <div className="max-auto text-center my-4 sm:my-6">
+                <h3 className="font-satisfy font-semibold text-lg sm:text-xl">
+                    By {storyDetails.tourist_name}
+                </h3>
+                <p className="my-1 sm:my-2">Travel Story Writer</p>
+                <p className="my-1 sm:my-2">
+                    {storyDetails.start_date} - {storyDetails.end_date}
+                </p>
+                <p className="my-1 sm:my-2">{storyDetails.destination}</p>
+                <div className="border-t border-t-yellow my-2 sm:my-3 w-3/4 sm:w-1/2 mx-auto"></div>
+                <div className="flex justify-around sm:justify-between w-4/5 sm:w-3/5 mx-auto my-3 sm:my-4 text-lg sm:text-xl">
+                    <p className="flex gap-2 items-center">
+                        <MdOutlineRemoveRedEye /> 2050
+                    </p>
+                    <button>
+                        <FacebookShareButton url={shareUrl}>
+                            <FacebookIcon size={30} sm:size={40} round />
+                        </FacebookShareButton>
+                    </button>
+                    <p className="flex gap-2 items-center">
+                        <IoChatbubbleOutline /> 150
+                    </p>
                 </div>
             </div>
-            <div className="max-w-7xl mx-auto">
-                <h3 className="text-xl font-maven">The Amazing Story :</h3>
-                <div className="border-t border-t-yellow my-3 w-52"></div>
-                <p className="text-lg my-3">History of {storyDetails.destination}</p>
+            <div className="max-w-full sm:max-w-7xl mx-auto px-4 sm:px-0">
+                <h3 className="text-lg sm:text-xl font-maven">
+                    The Amazing Story :
+                </h3>
+                <div className="border-t border-t-yellow my-2 sm:my-3 w-40 sm:w-52"></div>
+                <p className="text-base sm:text-lg my-2 sm:my-3">
+                    History of {storyDetails.destination}
+                </p>
                 <p>{storyDetails.description.history}</p>
                 <p>{storyDetails.description.geography}</p>
                 <p>{storyDetails.description.flora_and_fauna}</p>
                 <p>{storyDetails.description.activities}</p>
                 <p>{storyDetails.description.conservation}</p>
             </div>
-
         </div>
     );
 };
